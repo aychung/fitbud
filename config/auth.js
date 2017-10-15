93e00a2f7df177dc601ce744ee4bef6c22f5044d
Albert Chung <aychung86 (at) gmail.com>
2017-10-14 19:45:58 -0700

    add auth for heroku

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '281134505725053', // your App ID
+        'clientSecret'  : '7ca07f3736fea8746c22a0ec83a8f797', // your App Secret
+        'callbackURL'   : 'http://localhost:3001/auth/facebook/callback'
+    }
+  }
